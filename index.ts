49642858892795a87cc6921c6839718355ae3e25
Andre Harper <harperandre277@gmail.com>
2024-09-25 23:48:22 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express from "express"; 
+
+
+const app = express();
+
+app.get("/", (req, res) => res.send('Hello World, in TYPESCRIPT'));
+
+
+const PORT = 3000 
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
